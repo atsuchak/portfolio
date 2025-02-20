da0cfb947168bdb8833e255f792d9b22414c6874
atsuchak <suchak0630@gmail.com>
2025-02-20 16:31:59 +0600

    ok

--- scriptCalcu.js
+++ scriptCalcu.js
@@ -3,7 +3,7 @@ const display = document.getElementById("display");
 function appendToDisplay(input) {
     if(input === 'M+'){
         display.value = "Currenlty working on it";
-        display.style.fontSize = "2.5rem";
+        display.style.fontSize = "2rem";
     }else {
         display.value += input;    
     }
@@ -20,6 +20,6 @@ function calculate() {
 
 function specialDisplay() {
     display.value = "SAKIN MOGA";
-    display.style.fontSize = "4rem";
+    display.style.fontSize = "3rem";
     display.style.color = "red";
 }
