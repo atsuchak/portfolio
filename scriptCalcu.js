3e400f6600f692cf1da3d24afe40c8ea992c49c1
atsuchak <suchak0630@gmail.com>
2025-02-20 19:23:11 +0600

    sakin

--- scriptCalcu.js
+++ scriptCalcu.js
@@ -3,7 +3,6 @@ const display = document.getElementById("display");
 function appendToDisplay(input) {
     if(input === 'M+'){
         display.value = "Currenlty working on it";
-        display.style.fontSize = "5vh";
     }else {
         display.value += input;    
     }
@@ -26,6 +25,4 @@ function calculate() {
 
 function specialDisplay() {
     display.value = "SAKIN MOGA";
-    display.style.fontSize = "5vh";
-    display.style.color = "red";
 }
