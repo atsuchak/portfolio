288f30555b56f217ed8fa30b914dc5b54d2aa774
atsuchak <suchak0630@gmail.com>
2025-02-20 16:02:39 +0600

    update calculator

---
+++ scriptCalcu.js
@@ -0,0 +1,24 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    if(input === 'M+'){
+        display.value = "Currenlty working on it";
+        display.style.fontSize = "2.5rem";
+    }
+    display.value += input;    
+}
+
+function clearDisplay(input) {
+    display.value = "";
+    
+}
+
+function calculate() {
+    display.value = eval(display.value);
+}
+
+function specialDisplay() {
+    display.value = "SAKIN MOGA";
+    display.style.fontSize = "4rem";
+    display.style.color = "red";
+}
